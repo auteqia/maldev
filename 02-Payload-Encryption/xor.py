0ca577e93bdafb3c0f81a6435dc5e415078d3fc4
auteqia <feur@auteqia.garden>
2026-04-20 14:39:25 +0200

    xor builder in python

---
+++ 02-Payload-Encryption/xor.py
@@ -0,0 +1,16 @@
+with open("../shellcode/payload.bin", "rb") as f:
+    shellcode = bytearray(f.read())
+
+key = 0xAA
+final = 0
+
+for i in range(len(shellcode)):
+    shellcode[i] ^= key
+    final += shellcode[i]
+    print(f"0x{shellcode[i]:02X}, ", end="")
+
+
+with open("../shellcode/payload_enc.bin", "wb") as f:
+    f.write(shellcode)
+
+print("\nencrypted")
